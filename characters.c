b4ba6adfb43f0f1776d2ef44221b137b7d63a038
keruthika <30829818+keruthika@users.noreply.github.com>
2018-01-25 16:50:44 +0530

    Create characters.c

---
+++ characters.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+char a[100];
+printf("enter the character");
+scanf("%d",&a);
+for(i=0;i<=a;i++)
+{
+if(num=="")
+ count++;
+else
+printf("enter the number of characters");
+}
+return 0;
+}
