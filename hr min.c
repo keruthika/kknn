39eb770b450ed9bcaf429a922dcf200632facf07
keruthika <30829818+keruthika@users.noreply.github.com>
2018-01-25 16:38:24 +0530

    Create hr min.c

---
+++ hr min.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int hr1,min1,hr2,min2;
+int hr,min;
+printf("enter the hr:min value \n");
+scanf("%d",&hr1:min1);
+printf("enter the other hr:min value \n");
+scanf("%d",&hr2:min2);
+hr=hr1-hr2;
+min=min1-min2;
+printf("%d %d",hr,min);
+return 0;
+}
