c7b6fbd3629f9562178bf9078dc6633c2ab81497
keruthika <30829818+keruthika@users.noreply.github.com>
2018-01-25 15:52:29 +0530

    Create complexity analysis.c

---
+++ complexity analysis.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+inta[50],b,s;
+printf("%d",a);
+for(i=0;i<5;i++)
+{
+for(j=0;j<s;j++)
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+printf("%d"a[i]);
+scanf("%d",&a[j]);
+}}
+}
