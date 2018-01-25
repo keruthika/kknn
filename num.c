235971c822812102ec4bee17ad5e130c4a41672e
keruthika <30829818+keruthika@users.noreply.github.com>
2018-01-25 16:06:09 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[10],s,i;
+for(i=0;i<s[i]=\0;i++)
+{
+printf("%s",enter the string);
+scanf("%s",&s);
+if(i=0)
+print("no");
+else
+printf("yes");
+return 0;
+}
+}
